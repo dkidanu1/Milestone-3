7bd502d142a823c9d5040897dd63e683d81d1aa7
dkidanu1 <dkidanu@gmail.com>
2021-02-27 10:53:34 +0000

    Add recipe form

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,15 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $('.modal').modal();
     $('.collapsible').collapsible();
+    $('select').formSelect();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
 
 
