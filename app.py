f0f1988d939efb7ab98a0e108c7903ffb45f563d
dkidanu1 <dkidanu@gmail.com>
2021-02-27 09:57:34 +0000

    Create a Recipe function

--- app.py
+++ app.py
@@ -97,6 +97,12 @@ def signout():
     session.pop("user")
     return redirect(url_for("signin"))
 
+
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,8 @@
 // Mobile initialization from Materialize
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
+    $('.modal').modal();
+    $('.collapsible').collapsible();
 });
+
 
